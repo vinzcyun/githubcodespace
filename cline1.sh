0359bd25e58c027e4f6578093486ee776bad6155
duma <126393227+vinzcyun@users.noreply.github.com>
2024-05-12 21:00:05 +0700

    Update cline1.sh

--- cline1.sh
+++ cline1.sh
@@ -1,14 +1,16 @@
 sudo apt update
 apt install qemu-kvm -y
 qemu-img create -f raw filegz.img 20G
+clear
 read -p "Nhập link file Gz: " url && wget -O- --no-check-certificate "$url" | gunzip | dd of=filegz.img
 clear
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 sudo apt install ngrok -y
+clear
 read -p "Nhập authtoken ngrok: " token
-ngrok authtoken "$token"
-ngrok tcp 3389 --region ap &>/dev/null &
+ngrok authtoken "$token" && ngrok tcp 3389 --region ap &>/dev/null &
 sudo su
+clear
 read -p "Nhập dung lượng ổ đĩa: " disk_size
 qemu-img resize filegz.img "$disk_size"
 qemu-system-x86_64 \
