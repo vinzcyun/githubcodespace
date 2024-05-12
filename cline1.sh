ede405a3da5acc271cb3cbacd64a0d96617dfae1
duma <126393227+vinzcyun@users.noreply.github.com>
2024-05-12 20:20:19 +0700

    Update cline1.sh

--- cline1.sh
+++ cline1.sh
@@ -8,3 +8,13 @@ sudo apt install ngrok -y &&
 read -p "Nhập authtoken ngrok: " token && ngrok authtoken "$token" && ngrok tcp 3389 &>/dev/null &
 sudo su
 qemu-img resize filegz.img 1025G
+qemu-system-x86_64 \
+-net nic -net user,hostfwd=tcp::3389-:3389 \
+-m 12G -smp cores=4 \
+-cpu max \
+-enable-kvm \
+-boot order=d \
+-drive file=filegz.img,format=raw,if=virtio \
+-device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+-device usb-tablet \
+-vnc :0 -vga virtio
